c66a5fa69d06c9456d18ae02493359e8e2c00b30
bennettem <emily.pamelabennett@gmail.com>
2023-10-30 20:47:21 -0500

    starting structure of portfolio

---
+++ src/app/components/animation.tsx
@@ -0,0 +1,9 @@
+const Animation = () => {
+  return (
+    <div>
+      <h1>hey</h1>
+    </div>
+  );
+};
+
+export default Animation;
---
+++ src/app/components/openingScreen.tsx
@@ -0,0 +1,10 @@
+import Animation from "./Animation";
+const OpeningScreen = () => {
+  return (
+    <div className="opening-screen">
+      <Animation />
+    </div>
+  );
+};
+
+export default OpeningScreen;
